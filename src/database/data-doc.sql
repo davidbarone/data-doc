0bcf836dd0821025c9f9ee008e0ecd8cff29bdf2
davidbarone <davidbarone@live.com>
2021-03-07 18:32:44 +1100

    Updated database script

--- src/database/data-doc.sql
+++ src/database/data-doc.sql
@@ -1,7 +1,7 @@
 USE [master]
 GO
-/****** Object:  Database [MetadataRepository]    Script Date: 28/02/2021 8:05:39 PM ******/
-CREATE DATABASE [MetadataRepository]
+/****** Object:  Database [DataDoc]    Script Date: 7/03/2021 6:31:57 PM ******/
+CREATE DATABASE [DataDoc]
  CONTAINMENT = NONE
  ON  PRIMARY 
 ( NAME = N'MetadataRepository', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\MetadataRepository.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
@@ -9,88 +9,97 @@ CREATE DATABASE [MetadataRepository]
 ( NAME = N'MetadataRepository_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\MetadataRepository_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
  WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
-ALTER DATABASE [MetadataRepository] SET COMPATIBILITY_LEVEL = 150
+ALTER DATABASE [DataDoc] SET COMPATIBILITY_LEVEL = 150
 GO
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
-EXEC [MetadataRepository].[dbo].[sp_fulltext_database] @action = 'enable'
+EXEC [DataDoc].[dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
-ALTER DATABASE [MetadataRepository] SET ANSI_NULL_DEFAULT OFF 
+ALTER DATABASE [DataDoc] SET ANSI_NULL_DEFAULT OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET ANSI_NULLS OFF 
+ALTER DATABASE [DataDoc] SET ANSI_NULLS OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET ANSI_PADDING OFF 
+ALTER DATABASE [DataDoc] SET ANSI_PADDING OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET ANSI_WARNINGS OFF 
+ALTER DATABASE [DataDoc] SET ANSI_WARNINGS OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET ARITHABORT OFF 
+ALTER DATABASE [DataDoc] SET ARITHABORT OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET AUTO_CLOSE OFF 
+ALTER DATABASE [DataDoc] SET AUTO_CLOSE OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET AUTO_SHRINK OFF 
+ALTER DATABASE [DataDoc] SET AUTO_SHRINK OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET AUTO_UPDATE_STATISTICS ON 
+ALTER DATABASE [DataDoc] SET AUTO_UPDATE_STATISTICS ON 
 GO
-ALTER DATABASE [MetadataRepository] SET CURSOR_CLOSE_ON_COMMIT OFF 
+ALTER DATABASE [DataDoc] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET CURSOR_DEFAULT  GLOBAL 
+ALTER DATABASE [DataDoc] SET CURSOR_DEFAULT  GLOBAL 
 GO
-ALTER DATABASE [MetadataRepository] SET CONCAT_NULL_YIELDS_NULL OFF 
+ALTER DATABASE [DataDoc] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET NUMERIC_ROUNDABORT OFF 
+ALTER DATABASE [DataDoc] SET NUMERIC_ROUNDABORT OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET QUOTED_IDENTIFIER OFF 
+ALTER DATABASE [DataDoc] SET QUOTED_IDENTIFIER OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET RECURSIVE_TRIGGERS OFF 
+ALTER DATABASE [DataDoc] SET RECURSIVE_TRIGGERS OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET  DISABLE_BROKER 
+ALTER DATABASE [DataDoc] SET  DISABLE_BROKER 
 GO
-ALTER DATABASE [MetadataRepository] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+ALTER DATABASE [DataDoc] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET DATE_CORRELATION_OPTIMIZATION OFF 
+ALTER DATABASE [DataDoc] SET DATE_CORRELATION_OPTIMIZATION OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET TRUSTWORTHY OFF 
+ALTER DATABASE [DataDoc] SET TRUSTWORTHY OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+ALTER DATABASE [DataDoc] SET ALLOW_SNAPSHOT_ISOLATION OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET PARAMETERIZATION SIMPLE 
+ALTER DATABASE [DataDoc] SET PARAMETERIZATION SIMPLE 
 GO
-ALTER DATABASE [MetadataRepository] SET READ_COMMITTED_SNAPSHOT OFF 
+ALTER DATABASE [DataDoc] SET READ_COMMITTED_SNAPSHOT OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET HONOR_BROKER_PRIORITY OFF 
+ALTER DATABASE [DataDoc] SET HONOR_BROKER_PRIORITY OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET RECOVERY SIMPLE 
+ALTER DATABASE [DataDoc] SET RECOVERY SIMPLE 
 GO
-ALTER DATABASE [MetadataRepository] SET  MULTI_USER 
+ALTER DATABASE [DataDoc] SET  MULTI_USER 
 GO
-ALTER DATABASE [MetadataRepository] SET PAGE_VERIFY CHECKSUM  
+ALTER DATABASE [DataDoc] SET PAGE_VERIFY CHECKSUM  
 GO
-ALTER DATABASE [MetadataRepository] SET DB_CHAINING OFF 
+ALTER DATABASE [DataDoc] SET DB_CHAINING OFF 
 GO
-ALTER DATABASE [MetadataRepository] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+ALTER DATABASE [DataDoc] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
 GO
-ALTER DATABASE [MetadataRepository] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+ALTER DATABASE [DataDoc] SET TARGET_RECOVERY_TIME = 60 SECONDS 
 GO
-ALTER DATABASE [MetadataRepository] SET DELAYED_DURABILITY = DISABLED 
+ALTER DATABASE [DataDoc] SET DELAYED_DURABILITY = DISABLED 
 GO
-ALTER DATABASE [MetadataRepository] SET QUERY_STORE = OFF
+ALTER DATABASE [DataDoc] SET QUERY_STORE = OFF
 GO
-USE [MetadataRepository]
+USE [DataDoc]
 GO
-/****** Object:  Table [dbo].[Project]    Script Date: 28/02/2021 8:05:39 PM ******/
+/****** Object:  Table [dbo].[Project]    Script Date: 7/03/2021 6:31:57 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Project](
+	[ProjectId] [int] IDENTITY(1,1) NOT NULL,
 	[ProjectName] [varchar](50) NOT NULL,
+	[ProjectDesc] [varchar](1000) NULL,
 	[ConnectionString] [varchar](250) NOT NULL,
-	[Version] [int] NOT NULL,
-	[LastUpdated] [datetime2](7) NOT NULL
+	[ScanVersion] [int] NOT NULL,
+	[ScanUpdatedDt] [datetime2](7) NOT NULL,
+	[ConfigVersion] [int] NOT NULL,
+	[ConfigUpdatedDt] [datetime2](7) NOT NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED 
+(
+	[ProjectId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  View [dbo].[FactCruises]    Script Date: 28/02/2021 8:05:39 PM ******/
+/****** Object:  View [dbo].[FactCruises]    Script Date: 7/03/2021 6:31:57 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -99,13 +108,13 @@ CREATE VIEW [dbo].[FactCruises]
 AS
 	SELECT * FROM PROJECT
 GO
-/****** Object:  Table [dbo].[Attribute]    Script Date: 28/02/2021 8:05:39 PM ******/
+/****** Object:  Table [dbo].[Attribute]    Script Date: 7/03/2021 6:31:57 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Attribute](
-	[ProjectName] [varchar](250) NOT NULL,
+	[ProjectId] [int] NOT NULL,
 	[EntityName] [varchar](250) NOT NULL,
 	[AttributeName] [varchar](250) NOT NULL,
 	[Order] [int] NOT NULL,
@@ -113,16 +122,36 @@ CREATE TABLE [dbo].[Attribute](
 	[DataType] [varchar](9) NOT NULL,
 	[DataLength] [smallint] NOT NULL,
 	[Precision] [tinyint] NOT NULL,
-	[Scale] [tinyint] NOT NULL
+	[Scale] [tinyint] NOT NULL,
+	[IsNullable] [bit] NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Entity]    Script Date: 28/02/2021 8:05:39 PM ******/
+/****** Object:  Table [dbo].[AttributeConfig]    Script Date: 7/03/2021 6:31:57 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AttributeConfig](
+	[ProjectId] [int] NOT NULL,
+	[EntityName] [sysname] NOT NULL,
+	[AttributeName] [sysname] NOT NULL,
+	[AttributeDesc] [varchar](1000) NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_AttributeConfig] PRIMARY KEY CLUSTERED 
+(
+	[ProjectId] ASC,
+	[EntityName] ASC,
+	[AttributeName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Entity]    Script Date: 7/03/2021 6:31:57 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Entity](
-	[ProjectName] [varchar](50) NOT NULL,
+	[ProjectId] [int] NOT NULL,
 	[EntityName] [sysname] NOT NULL,
 	[EntityType] [varchar](50) NOT NULL,
 	[RowCount] [int] NULL,
@@ -132,25 +161,42 @@ CREATE TABLE [dbo].[Entity](
 	[Definition] [varchar](max) NULL,
  CONSTRAINT [PK_SERVER_OBJECT] PRIMARY KEY CLUSTERED 
 (
-	[ProjectName] ASC,
+	[ProjectId] ASC,
 	[EntityName] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[EntityPublication]    Script Date: 28/02/2021 8:05:39 PM ******/
+/****** Object:  Table [dbo].[EntityConfig]    Script Date: 7/03/2021 6:31:57 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[EntityPublication](
-	[ProjectName] [varchar](50) NOT NULL,
+CREATE TABLE [dbo].[EntityConfig](
+	[ProjectId] [int] NOT NULL,
 	[EntityName] [sysname] NOT NULL,
 	[EntityAlias] [varchar](50) NOT NULL,
-	[EntityDescription] [varchar](50) NOT NULL,
-	[IsPublished] [bit] NOT NULL
+	[EntityDesc] [varchar](1000) NOT NULL,
+	[ShowData] [bit] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_EntityConfig] PRIMARY KEY CLUSTERED 
+(
+	[ProjectId] ASC,
+	[EntityName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[EntityDependency]    Script Date: 7/03/2021 6:31:57 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EntityDependency](
+	[ProjectId] [int] NOT NULL,
+	[ParentEntityName] [sysname] NOT NULL,
+	[ChildEntityName] [sysname] NOT NULL
 ) ON [PRIMARY]
 GO
 USE [master]
 GO
-ALTER DATABASE [MetadataRepository] SET  READ_WRITE 
+ALTER DATABASE [DataDoc] SET  READ_WRITE 
 GO
