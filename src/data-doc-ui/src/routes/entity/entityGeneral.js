7604813e718fca185bf6e72264a7812941b00d27
davidbarone <davidbarone@live.com>
2021-03-28 19:52:41 +1100

    Added attribute and relationship tabs on entity.

---
+++ src/data-doc-ui/src/routes/entity/entityGeneral.js
@@ -0,0 +1,107 @@
+import { h } from "preact";
+import style from "./style.css";
+import { useState, useEffect } from "preact/hooks";
+import { getEntity, updateEntity } from "../../utils/apiFacade";
+
+const EntityGeneral = ({ projectId, entityName }) => {
+  const [entity, setEntity] = useState({});
+
+  useEffect(() => {
+    getEntity(projectId, entityName).then((e) => setEntity(e));
+  }, []);
+
+  const onInput = (e) => {
+    const val =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    const newValue = { ...entity, [e.target.name]: val };
+    console.log(newValue);
+    setEntity(newValue);
+  };
+
+  const onSubmit = (e) => {
+    updateEntity(projectId, entityName, entity);
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h3>General Information</h3>
+
+      <form onSubmit={onSubmit}>
+        <div class={style.field}>
+          <label>Entity Name:</label>
+          <input
+            readOnly
+            type="text"
+            name="entityName"
+            value={entity.entityName}
+          />
+        </div>
+
+        <div class={style.field}>
+          <label>Entity Alias:</label>
+          <input
+            type="text"
+            name="entityAlias"
+            value={entity.entityAlias}
+            onInput={onInput}
+          />
+        </div>
+
+        <div class={style.field}>
+          <label>Entity Description:</label>
+          <input
+            type="text"
+            name="entityDesc"
+            value={entity.entityDesc}
+            onInput={onInput}
+          />
+        </div>
+
+        <div class={style.field}>
+          <label>Entity Comment:</label>
+          <textarea
+            name="entityComment"
+            value={entity.entityComment}
+            onInput={onInput}
+          />
+        </div>
+
+        <div class={style.field}>
+          <label>Show Data?</label>
+          <input
+            type="checkbox"
+            name="showData"
+            checked={entity.showData}
+            onClick={onInput}
+          />
+        </div>
+
+        <div class={style.field}>
+          <label>Show Definition?</label>
+          <input
+            type="checkbox"
+            name="showDefinition"
+            checked={entity.showDefinition}
+            onClick={onInput}
+          />
+        </div>
+
+        <div class={style.field}>
+          <label>Active?</label>
+          <input
+            type="checkbox"
+            name="isActive"
+            checked={entity.isActive}
+            onClick={onInput}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+        <a href="/projects">Back to projects</a>
+      </form>
+    </div>
+  );
+};
+
+export default EntityGeneral;
--- src/data-doc-ui/src/routes/entity/index.js
+++ src/data-doc-ui/src/routes/entity/index.js
@@ -1,104 +1,28 @@
 import { h } from "preact";
 import style from "./style.css";
-import { getEntity, updateEntity } from "../../utils/apiFacade";
-import { useState, useEffect } from "preact/hooks";
+import { useState } from "preact/hooks";
+import { Tabs } from "../../components/tabs/tabs";
+import { Tab } from "../../components/tabs/tab";
+import EntityGeneral from "./entityGeneral";
 
 const Entity = ({ projectId, entityName }) => {
-  const [entity, setEntity] = useState({});
+  const [tabIndex, setTabIndex] = useState(0);
 
-  useEffect(() => {
-    getEntity(projectId, entityName).then((e) => setEntity(e));
-  }, []);
-
-  const onInput = (e) => {
-    const val =
-      e.target.type === "checkbox" ? e.target.checked : e.target.value;
-    const newValue = { ...entity, [e.target.name]: val };
-    console.log(newValue);
-    setEntity(newValue);
-  };
-
-  const onSubmit = (e) => {
-    updateEntity(projectId, entityName, entity);
-    e.preventDefault();
+  const setTab = (t) => {
+    setTabIndex(t);
   };
 
   return (
     <div class={style.home}>
       <h3>Entity Information</h3>
-      <form onSubmit={onSubmit}>
-        <div class={style.field}>
-          <label>Entity Name:</label>
-          <input
-            readOnly
-            type="text"
-            name="entityName"
-            value={entity.entityName}
-          />
-        </div>
 
-        <div class={style.field}>
-          <label>Entity Alias:</label>
-          <input
-            type="text"
-            name="entityAlias"
-            value={entity.entityAlias}
-            onInput={onInput}
-          />
-        </div>
-
-        <div class={style.field}>
-          <label>Entity Description:</label>
-          <input
-            type="text"
-            name="entityDesc"
-            value={entity.entityDesc}
-            onInput={onInput}
-          />
-        </div>
-
-        <div class={style.field}>
-          <label>Entity Comment:</label>
-          <textarea
-            name="entityComment"
-            value={entity.entityComment}
-            onInput={onInput}
-          />
-        </div>
-
-        <div class={style.field}>
-          <label>Show Data?</label>
-          <input
-            type="checkbox"
-            name="showData"
-            checked={entity.showData}
-            onClick={onInput}
-          />
-        </div>
-
-        <div class={style.field}>
-          <label>Show Definition?</label>
-          <input
-            type="checkbox"
-            name="showDefinition"
-            checked={entity.showDefinition}
-            onClick={onInput}
-          />
-        </div>
-
-        <div class={style.field}>
-          <label>Active?</label>
-          <input
-            type="checkbox"
-            name="isActive"
-            checked={entity.isActive}
-            onClick={onInput}
-          />
-        </div>
-
-        <button type="submit">Submit</button>
-        <a href="/projects">Back to projects</a>
-      </form>
+      <Tabs activeTab={tabIndex} onChangeTab={setTab}>
+        <Tab title="General">
+          <EntityGeneral projectId={projectId} entityName={entityName} />
+        </Tab>
+        <Tab title="Attributes">To Do</Tab>
+        <Tab title="Relationships">To Do</Tab>
+      </Tabs>
     </div>
   );
 };
